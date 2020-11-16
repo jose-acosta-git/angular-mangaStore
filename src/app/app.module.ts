04456b9256066b970d354910ccd660194d15348d
jose-acosta-git <jose.acosta.git@gmail.com>
2020-11-15 22:13:31 -0300

    agrega elementos estáticos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BookListComponent } from './book-list/book-list.component';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
+import { MangaStoreBooksComponent } from './manga-store-books/manga-store-books.component';
+import { MangaStoreAboutComponent } from './manga-store-about/manga-store-about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BookListComponent,
+    ShoppingCartComponent,
+    MangaStoreBooksComponent,
+    MangaStoreAboutComponent
   ],
   imports: [
     BrowserModule,
